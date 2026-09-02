570c4ce2811059fafd5ffe25a4849422c854a886
구정아 <jeongkkt@gmail.com>
2026-09-02 13:16:43 +0900

    fix: 팀생성페이지 비로그인 열람 가능하도록 수정

--- FE/src/pages/TeamCreatePage/TeamCreatePage.jsx
+++ FE/src/pages/TeamCreatePage/TeamCreatePage.jsx
@@ -1,4 +1,4 @@
-﻿﻿import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logoIcon from "../../assets/logoIcon.svg";
 import FormActions from "./components/FormActions.jsx";
